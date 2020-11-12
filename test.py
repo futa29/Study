29f9bb61cc20b0c9c010e6d4da33279ebc242177
futa29 <fu50ta@ezweb.ne.jp>
2020-11-13 02:43:55 +0900

    課題

---
+++ test.py
@@ -0,0 +1,62 @@
+import tkinter as tk
+from tkinter import messagebox
+#import winsound
+
+
+#最初の画面         
+base = tk.Tk()
+base.geometry("800x700")
+base.title("占いアプリ")
+input_label = tk.Button(base,text="ド",width=8,height=10)
+input_label.place(x=70, y=300)
+input_label_ = tk.Button(base,text="ド＃",width=8,height=10)
+input_label_.place(x=105, y=140)
+input_label2 = tk.Button(base,text="レ",width=8,height=10)
+input_label2.place(x=140, y=300)
+input_label2_ = tk.Button(base,text="レ#",width=8,height=10)
+input_label2_.place(x=175, y=140)
+input_label3 = tk.Button(base,text="ミ",width=8,height=10)
+input_label3.place(x=210, y=300)
+input_label4 = tk.Button(base,text="ファ",width=8,height=10)
+input_label4.place(x=280, y=300)
+input_label4_ = tk.Button(base,text="ファ#",width=8,height=10)
+input_label4_.place(x=320, y=140)
+input_label5 = tk.Button(base,text="ソ",width=8,height=10)
+input_label5.place(x=350, y=300)
+input_label5_ = tk.Button(base,text="ソ#",width=8,height=10)
+input_label5_.place(x=390, y=140)
+input_label6 = tk.Button(base,text="ラ",width=8,height=10)
+input_label6.place(x=420, y=300)
+input_label6_ = tk.Button(base,text="ラ#",width=8,height=10)
+input_label6_.place(x=460, y=140)
+input_label7 = tk.Button(base,text="シ",width=8,height=10)
+input_label7.place(x=490, y=300)
+input_label8 = tk.Button(base,text="ド",width=8,height=10)
+input_label8.place(x=560, y=300)
+base.mainloop()
+import tkinter as tk
+from tkinter import messagebox
+import winsound
+
+
+#最初の画面         
+base = tk.Tk()
+base.geometry("800x700")
+base.title("ピアノ")
+input_label = tk.Button(base,text="ド",width=10,height=10)
+input_label.place(x=70, y=300)
+input_label2 = tk.Button(base,text="レ",width=10,height=10)
+input_label2.place(x=140, y=300)
+input_label3 = tk.Button(base,text="ミ",width=10,height=10)
+input_label3.place(x=210, y=300)
+input_label4 = tk.Button(base,text="ファ",width=10,height=10)
+input_label4.place(x=280, y=300)
+input_label5 = tk.Button(base,text="ソ",width=10,height=10)
+input_label5.place(x=350, y=300)
+input_label6 = tk.Button(base,text="ラ",width=10,height=10)
+input_label6.place(x=420, y=300)
+input_label7 = tk.Button(base,text="シ",width=10,height=10)
+input_label7.place(x=490, y=300)
+input_label8 = tk.Button(base,text="ド",width=10,height=10)
+input_label8.place(x=560, y=300)
+base.mainloop()
